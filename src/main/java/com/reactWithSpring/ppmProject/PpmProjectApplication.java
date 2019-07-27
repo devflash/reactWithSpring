6c77c73455f31128e6d4030ebd68af7303fa3346
Mrugesh Wadagbalkar <mrugeshwadagbalkar@gmail.com>
2019-07-27 12:25:49 +0530

    First commit

---
+++ src/main/java/com/reactWithSpring/ppmProject/PpmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.reactWithSpring.ppmProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactWithSpring/ppmProject/PpmProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reactWithSpring.ppmProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
